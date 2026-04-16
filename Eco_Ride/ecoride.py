18ab6363b0d19a2d2c4d9fd338c0ecf835cc23fb
anzarahmed45 <ansarnazar508@gmail.com>
2026-04-16 11:30:08 +0530

    UC1: Basic fleet setup with Vehicle class

--- Eco_Ride/ecoride.py
+++ Eco_Ride/ecoride.py
@@ -6,6 +6,23 @@ class EcoRideMain:
         print("=" * 50)
 
 
-# Entry point
 if __name__ == "__main__":
     EcoRideMain.start()
+
+
+
+class Vehicle:
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = battery_percentage
+
+    def display(self):
+        print("\n--- Vehicle Details ---")
+        print("Vehicle ID      :", self.vehicle_id)
+        print("Model           :", self.model)
+        print("Battery (%)     :", self.battery_percentage)
+
+
+v1 = Vehicle("KL-70A-0369", "BMW E5", 80)
+v1.display()
