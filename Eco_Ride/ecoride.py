a430dd30027c4aabf0ce28cce5ed7ee6982da708
anzarahmed45 <ansarnazar508@gmail.com>
2026-04-17 00:59:46 +0530

    UC9: Implemented categorized view grouping vehicles by type

--- Eco_Ride/ecoride.py
+++ Eco_Ride/ecoride.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class EcoRideMain:
     @staticmethod
     def start():
@@ -8,3 +10,190 @@ class EcoRideMain:
 
 if __name__ == "__main__":
     EcoRideMain.start()
+
+
+
+class Vehicle(ABC):
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+
+        self.__battery_percentage = None
+        self.__maintenance_status = "Good"
+        self.__rental_price = 0
+
+        self.set_battery(battery_percentage)
+
+
+    def get_battery(self):
+        return self.__battery_percentage
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def get_rental_price(self):
+        return self.__rental_price
+
+
+    def set_battery(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            print("Invalid battery percentage! (0-100 only)")
+
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    def set_rental_price(self, rental_price):
+        if rental_price >= 0:
+            self.__rental_price = rental_price
+        else:
+            print("Rental price cannot be negative!")
+
+    @abstractmethod
+    def calculate_trip_cost(self, value):
+        pass
+
+
+    def display(self):
+        print("\n--- Vehicle Details ---")
+        print("Vehicle ID      :", self.vehicle_id)
+        print("Model           :", self.model)
+        print("Battery (%)     :", self.__battery_percentage)
+        print("Maintenance     :", self.__maintenance_status)
+        print("Rental Price    : $", self.__rental_price)
+
+
+    def __eq__(self, other):
+        if isinstance(other, Vehicle):
+            return self.vehicle_id == other.vehicle_id
+        return False
+
+
+class FleetManager:
+    def __init__(self):
+        self.fleet_hubs = {}   
+
+    def add_hub(self, hub_name):
+        if hub_name not in self.fleet_hubs:
+            self.fleet_hubs[hub_name] = []
+            print(f"Hub '{hub_name}' added.")
+        else:
+            print(f"Hub '{hub_name}' already exists.")
+
+    def add_vehicle(self, hub_name, vehicle):
+            if hub_name in self.fleet_hubs:
+                if any(v.vehicle_id == vehicle.vehicle_id for v in self.fleet_hubs[hub_name]):
+                    print(f"Duplicate Vehicle ID '{vehicle.vehicle_id}' not allowed in {hub_name}.")
+                else:
+                    self.fleet_hubs[hub_name].append(vehicle)
+                    print(f"Vehicle {vehicle.vehicle_id} added to {hub_name}.")
+            else:
+                print(f"Hub '{hub_name}' not found.")
+
+    def display_hubs(self):
+        print("\n========== FLEET HUBS ==========")
+        for hub, vehicles in self.fleet_hubs.items():
+            print(f"\nHub: {hub}")
+            if not vehicles:
+                print("  No vehicles available")
+            else:
+                for v in vehicles:
+                    print(f"  - {v.vehicle_id} | {v.model}")
+
+
+    def search_by_hub(self, hub_name):
+        if hub_name in self.fleet_hubs:
+            print(f"\nVehicles in Hub: {hub_name}")
+            for v in self.fleet_hubs[hub_name]:
+                print(f"  - {v.vehicle_id} | {v.model} | Battery: {v.get_battery()}%")
+        else:
+            print(f"Hub '{hub_name}' not found.")
+
+    def search_high_battery(self, threshold=80):
+        print(f"\nVehicles with Battery > {threshold}%:")
+
+        all_vehicles = [v for vehicles in self.fleet_hubs.values() for v in vehicles]
+
+        result = list(filter(lambda v: v.get_battery() > threshold, all_vehicles))
+
+        if result:
+            for v in result:
+                print(f"  - {v.vehicle_id} | {v.model} | Battery: {v.get_battery()}%")
+        else:
+            print("No vehicles found with high battery.")
+
+    def categorize_vehicles(self):
+        categorized = {
+            "Car": [],
+            "Scooter": []
+        }
+
+        for vehicles in self.fleet_hubs.values():
+            for v in vehicles:
+                if isinstance(v, ElectricCar):
+                    categorized["Car"].append(v)
+                elif isinstance(v, ElectricScooter):
+                    categorized["Scooter"].append(v)
+
+        print("\n========== CATEGORIZED VEHICLES ==========")
+
+        print("\nCars:")
+        if categorized["Car"]:
+            for v in categorized["Car"]:
+                print(f"  - {v.vehicle_id} | {v.model}")
+        else:
+            print("  No cars available")
+
+        print("\nScooters:")
+        if categorized["Scooter"]:
+            for v in categorized["Scooter"]:
+                print(f"  - {v.vehicle_id} | {v.model}")
+        else:
+            print("  No scooters available")
+
+    
+
+
+class ElectricCar(Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
+        super().__init__(vehicle_id, model, battery_percentage)
+        self.seating_capacity = seating_capacity
+
+    def calculate_trip_cost(self, distance):
+        return 5 + (0.5 * distance)
+
+    def display(self):
+        super().display()
+        print("Type            : Car")
+        print("Seating Capacity:", self.seating_capacity)
+
+
+class ElectricScooter(Vehicle):
+    def __init__(self, vehicle_id, model, battery_percentage, max_speed_limit):
+        super().__init__(vehicle_id, model, battery_percentage)
+        self.max_speed_limit = max_speed_limit
+
+    def calculate_trip_cost(self, minutes):
+        return 1 + (0.15 * minutes)
+
+    def display(self):
+        super().display()
+        print("Type            : Scooter")
+        print("Max Speed Limit :", self.max_speed_limit)
+
+
+manager = FleetManager()
+
+car1 = ElectricCar("KL-70A-0369", "BMW E5", 85, 5)
+car2 = ElectricCar("KL-70B-0456", "Mercedez E7", 70, 5)
+scooter1 = ElectricScooter("KL-70B-0369", "Ather 450X", 90, 90)
+
+manager.add_hub("Downtown")
+
+manager.add_vehicle("Downtown", car1)
+manager.add_vehicle("Downtown", car2)
+manager.add_vehicle("Downtown", scooter1)
+
+manager.categorize_vehicles()
+    
