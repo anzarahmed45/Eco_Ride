f435e573b152482c0dd8aa9ceab22ef4a1f12b44
anzarahmed45 <ansarnazar508@gmail.com>
2026-04-16 10:52:58 +0530

    displaying page added

---
+++ Eco_Ride/ecoride.py
@@ -0,0 +1,11 @@
+class EcoRideMain:
+    @staticmethod
+    def start():
+        print("=" * 50)
+        print(" Welcome to Eco-Ride Urban Mobility System ")
+        print("=" * 50)
+
+
+# Entry point
+if __name__ == "__main__":
+    EcoRideMain.start()
